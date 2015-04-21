cb128c9ea94ffac7947ce91dc3de15a94b72b28e
henrique-correa <darthhenrique@gmail.com>
2015-04-21 18:05:41 -0300

    21/04/2015   18:05

---
+++ ShooterBalanciamento/Assets/PlanetConqueror/Jogador.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jogador : MonoBehaviour {
+
+	[SerializeField]float velocidade_X;
+	[SerializeField]float velocidade_Y;
+	[SerializeField]float vida;
+	[SerializeField]float dano;
+	[SerializeField]float Jogador_velocidade_tiro;
+	[SerializeField]float Jogador_cadencia_tiro;
+	float Jogador_proximo_tiro = 0.0f;
+	float move_X;
+	float move_Y;
+	GameObject Jog;
+
+
+	// Use this for initialization
+	void Start () {
+		Jog = GameObject.FindGameObjectWithTag("Player");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Jogador_move();
+		if(Input.GetKey(KeyCode.Space)){
+			Jogdor_atira();
+
+		}
+
+	
+	}
+
+
+	void Jogador_move(){
+		move_X = Input.GetAxis("Horizontal") * velocidade_X;
+		transform.Translate(move_X,0,0);
+		
+		move_Y = Input.GetAxis("Vertical") * velocidade_Y;
+		transform.Translate(0,0,move_Y);
+
+	}
+
+	void Jogdor_atira(){
+		if(Time.time > Jogador_proximo_tiro){
+			Jogador_proximo_tiro = Time.time + Jogador_cadencia_tiro;
+			GameObject Jogador_tiro = Instantiate(Resources.Load("Jogador_tiro"),transform.position,Quaternion.identity) as GameObject;
+			Physics.IgnoreCollision(gameObject.GetComponent<Rigidbody>().GetComponent<Collider>() , Jogador_tiro.GetComponent<Rigidbody>().GetComponent<Collider>());
+			Jogador_tiro.GetComponent<Rigidbody>().velocity = Jog.GetComponent<Rigidbody>().velocity;
+			Jogador_tiro.GetComponent<Rigidbody>().velocity += new Vector3(0.0f,0.0f,1.0f) * Jogador_velocidade_tiro;
+			//Jogador_tiro.GetComponent<Rigidbody>().velocity = new Vector3 (0.0f,0.0f,1.0f) * Jogador_velocidade_tiro;
+		}
+	}
+}
+
+
